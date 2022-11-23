49bb30f61a80f0237877431f4f4b043ca58a1dd6
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-11-23 21:26:14 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const filters = 'name.official,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-    fetch(`${BASE_URL}${name}?fields=${filters}`)
+    return fetch(`${BASE_URL}${name}?fields=${filters}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText)
