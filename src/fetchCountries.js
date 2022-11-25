f243222575a51a49ec7fb24fd0c1b114ae64dae6
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-11-25 16:27:09 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,7 +5,7 @@ export function fetchCountries(name) {
     return fetch(`${BASE_URL}${name}?fields=${filters}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.statusText)
+                throw new Error("Something went wrong")
             }
             return response.json();
         })
